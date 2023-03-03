542712f3dd7d17bac9ca42b60fa5b1f14fe564a0
thenuka99 <janith.h@creativesoftware.com>
2023-03-03 10:12:19 +0530

    int project

---
+++ src/test/java/com/healthCare/appointmentmanagement/AppointmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthCare.appointmentmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
