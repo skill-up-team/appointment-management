488f3abdabba0913720d58a8d858805e27c9ac25
thenuka99 <janith.h@creativesoftware.com>
2023-03-09 16:58:09 +0530

    DTO #1

--- src/main/java/com/healthCare/appointmentmanagement/dto/DoctorAppointmentDto.java
+++ src/main/java/com/healthCare/appointmentmanagement/dto/DoctorAppointmentDto.java
@@ -1,2 +1,18 @@
-package com.healthCare.appointmentmanagement.dto;public class DoctorAppointmentDto {
+package com.healthCare.appointmentmanagement.dto;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+
+import java.sql.Time;
+import java.util.Date;
+
+@Data
+public class DoctorAppointmentDto {
+    private Long docAppointmentId;
+    private String doctorId;
+    private String hospitalId;
+    private String patientId;
+    private String status;
+    private Date date;
+    private Time time;
 }
--- src/main/java/com/healthCare/appointmentmanagement/dto/LabAppointmentDto.java
+++ src/main/java/com/healthCare/appointmentmanagement/dto/LabAppointmentDto.java
@@ -1,2 +1,16 @@
-package com.healthCare.appointmentmanagement.dto;public class LabAppointmentDto {
+package com.healthCare.appointmentmanagement.dto;
+
+import jakarta.persistence.Column;
+import lombok.Data;
+
+import java.sql.Time;
+import java.util.Date;
+@Data
+public class LabAppointmentDto {
+    private Long labAppointmentId;
+    private String labId;
+    private String serviceId;
+    private String patientId;
+    private Date date;
+    private Time time;
 }
