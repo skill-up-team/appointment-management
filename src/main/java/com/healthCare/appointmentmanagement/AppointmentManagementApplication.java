ecf564267f1c79e6dcaa9de552e36ca27dd27c85
thenuka99 <janith.h@creativesoftware.com>
2023-03-14 10:32:18 +0530

    add model mapper bean

--- src/main/java/com/healthCare/appointmentmanagement/AppointmentManagementApplication.java
+++ src/main/java/com/healthCare/appointmentmanagement/AppointmentManagementApplication.java
@@ -1,13 +1,26 @@
 package com.healthCare.appointmentmanagement;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AppointmentManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppointmentManagementApplication.class, args);
 	}
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "world") String name){
+		return String.format("Hello, %s", name);
+	}
 }
