e12fc9486302b21ae1bc15dde8f9d9059f6596ef
thenuka99 <janith.h@creativesoftware.com>
2023-03-14 10:33:29 +0530

    add dto

--- src/main/java/com/healthCare/appointmentmanagement/dto/DoctorAppointmentDto.java
+++ src/main/java/com/healthCare/appointmentmanagement/dto/DoctorAppointmentDto.java
@@ -1,14 +1,20 @@
 package com.healthCare.appointmentmanagement.dto;
 
 import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Time;
 import java.util.Date;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class DoctorAppointmentDto {
-    private Long docAppointmentId;
+    private Long id;
     private String doctorId;
     private String hospitalId;
     private String patientId;
@@ -16,3 +22,4 @@ public class DoctorAppointmentDto {
     private Date date;
     private Time time;
 }
+
--- src/main/java/com/healthCare/appointmentmanagement/dto/LabAppointmentDto.java
+++ src/main/java/com/healthCare/appointmentmanagement/dto/LabAppointmentDto.java
@@ -1,13 +1,19 @@
 package com.healthCare.appointmentmanagement.dto;
 
 import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.sql.Time;
 import java.util.Date;
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class LabAppointmentDto {
-    private Long labAppointmentId;
+    private Long id;
     private String labId;
     private String serviceId;
     private String patientId;
