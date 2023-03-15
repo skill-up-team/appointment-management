1bb99ebb5e9318cac49322445caeceeb85937bd6
thenuka99 <janith.h@creativesoftware.com>
2023-03-15 14:57:48 +0530

    error handeling

--- src/main/java/com/healthCare/appointmentmanagement/appointmentManagementApplication.java
+++ src/main/java/com/healthCare/appointmentmanagement/appointmentManagementApplication.java
@@ -1,14 +1,14 @@
-package com.healthcare.Appointmentmanagement;
+package com.healthcare.appointmentmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.modelmapper.ModelMapper;
 @SpringBootApplication
-public class AppointmentManagementApplication {
+public class appointmentManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AppointmentManagementApplication.class, args);
+		SpringApplication.run(appointmentManagementApplication.class, args);
 	}
 	@Bean
 	public ModelMapper modelMapper() {
